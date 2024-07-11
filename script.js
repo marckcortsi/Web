fd44f2f6a85318267a4a4f5426bd886cabc6bee1
marckcortsi <113724635+marckcortsi@users.noreply.github.com>
2024-07-10 22:38:30 -0600

    Actualizar script.js

--- script.js
+++ script.js
@@ -2,15 +2,14 @@ const formIds = {
     entrega: 'formEntrega',
     finalizado: 'formFinalizado'
 };
-
 function mostrarFormulario(opcion) {
     document.querySelectorAll('.registro-form').forEach(form => form.classList.remove('active'));
     document.getElementById('registros').classList.remove('active');
-    document.getElementById('opciones').style.display = 'none';
-    document.getElementById('tituloPrincipal').style.display = 'none';
+    document.getElementById('opciones').classList.add('hidden');
+    document.getElementById('tituloPrincipal').classList.add('hidden');
     document.getElementById(`registro${capitalize(opcion)}`).classList.add('active');
+    document.getElementById('resumenTitulo').classList.add('hidden');  // Ocultar el título
 }
-
 function registrar(opcion) {
     const form = document.getElementById(formIds[opcion]);
     const nombre = form.querySelector('select[id^="nombre"]').value;
@@ -43,13 +42,12 @@ function registrar(opcion) {
 
     form.reset();
     mostrarMensaje(opcion, 'El registro se realizó exitosamente.');
+    actualizarTablaRegistros();  // Llamar a la función después de registrar un nuevo pedido
 }
-
 function verificarDuplicado(opcion, pedido) {
     let registros = JSON.parse(localStorage.getItem(opcion)) || [];
     return registros.some(registro => registro.pedido.toUpperCase() === pedido.toUpperCase());
 }
-
 function mostrarAlerta(opcion) {
     const mensaje = opcion === 'entrega'
         ? "ADVERTENCIA EL PEDIDO QUE ESTA INGRESANDO YA HA SIDO REGISTRADO CON ANTERIORIDAD, FAVOR DE VALIDAR QUE EL PEDIDO NO HAYA SIDO SURTIDO DOS VECES"
@@ -57,24 +55,23 @@ function mostrarAlerta(opcion) {
     document.getElementById('alertaMensaje').innerText = mensaje;
     document.getElementById('alertaDuplicado').classList.add('active');
 }
-
 function cerrarAlerta() {
     document.getElementById('alertaDuplicado').classList.remove('active');
 }
-
 function cancelarRegistro() {
     document.querySelectorAll('.registro-form').forEach(form => form.classList.remove('active'));
-    document.getElementById('opciones').style.display = 'flex';
-    document.getElementById('tituloPrincipal').style.display = 'block';
+    document.getElementById('registros').classList.remove('active');
+    document.getElementById('opciones').classList.remove('hidden');
+    document.getElementById('tituloPrincipal').classList.remove('hidden');
+    document.getElementById('resumenTitulo').classList.remove('hidden');  // Mostrar el título
 }
-
 function mostrarTodosRegistros() {
     mostrarRegistros();
     document.getElementById('registros').classList.add('active');
-    document.getElementById('opciones').style.display = 'none';
-    document.getElementById('tituloPrincipal').style.display = 'none';
+    document.getElementById('opciones').classList.add('hidden');
+    document.getElementById('tituloPrincipal').classList.add('hidden');
+    document.getElementById('resumenTitulo').classList.add('hidden');  // Ocultar el título
 }
-
 function mostrarRegistros() {
     const tipos = ['entrega', 'finalizado'];
     let listaHTML = '';
@@ -98,7 +95,6 @@ function mostrarRegistros() {
 
     document.getElementById('listaRegistros').innerHTML = listaHTML;
 }
-
 function buscarPorPedido() {
     const pedidoBusqueda = document.getElementById('pedidoBusqueda').value.trim().toUpperCase();
     if (!pedidoBusqueda) return;
@@ -125,19 +121,15 @@ function buscarPorPedido() {
 
     document.getElementById('listaRegistros').innerHTML = listaHTML;
 }
-
 function borrarFiltro() {
     document.getElementById('pedidoBusqueda').value = '';
     mostrarRegistros();
-}
-
-function calcularTiempoTotal(fechaInicio, fechaFin) {
+}function calcularTiempoTotal(fechaInicio, fechaFin) {
     const diff = fechaFin - fechaInicio;
     const hours = Math.floor(diff / (1000 * 60 * 60));
     const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
     return `${hours} horas y ${minutes} minutos`;
 }
-
 function formatDateTime(date) {
     return new Intl.DateTimeFormat('es-ES', {
         day: '2-digit', 
@@ -149,11 +141,9 @@ function formatDateTime(date) {
         hour12: true 
     }).format(date);
 }
-
 function capitalize(str) {
     return str.charAt(0).toUpperCase() + str.slice(1);
 }
-
 function exportarRegistros() {
     const fechaInicio = document.getElementById('fechaInicio').value;
     const fechaFin = document.getElementById('fechaFin').value;
@@ -167,14 +157,8 @@ function exportarRegistros() {
     const fechaFinObj = new Date(fechaFin);
     fechaFinObj.setHours(23, 59, 59, 999); // Asegurarse de incluir todo el día de la fecha final
 
-    console.log('Fecha de inicio seleccionada:', fechaInicioObj);
-    console.log('Fecha de fin seleccionada:', fechaFinObj);
-
     const entregaRegistros = JSON.parse(localStorage.getItem('entrega')) || [];
     const finalizadoRegistros = JSON.parse(localStorage.getItem('finalizado')) || [];
-
-    console.log('Registros de entrega:', entregaRegistros);
-    console.log('Registros finalizados:', finalizadoRegistros);
 
     const wb = XLSX.utils.book_new();
 
@@ -188,8 +172,6 @@ function exportarRegistros() {
 
     // Filtrar registros de entrega por rango de fechas
     const entregaFiltrados = filtrarRegistrosPorFecha(entregaRegistros, fechaInicioObj, fechaFinObj);
-
-    console.log('Registros de entrega filtrados:', entregaFiltrados);
 
     if (entregaFiltrados.length > 0) {
         // Crear hoja para registros de entrega
@@ -208,8 +190,6 @@ function exportarRegistros() {
 
     // Filtrar registros de finalizado por rango de fechas
     const finalizadoFiltrados = filtrarRegistrosPorFecha(finalizadoRegistros, fechaInicioObj, fechaFinObj);
-
-    console.log('Registros finalizados filtrados:', finalizadoFiltrados);
 
     if (finalizadoFiltrados.length > 0) {
         // Crear hoja para registros de finalizado
@@ -239,21 +219,19 @@ function exportarRegistros() {
 
     XLSX.writeFile(wb, nombreArchivo);
 }
-
 function formatDate(date) {
     const day = String(date.getDate()).padStart(2, '0');
     const month = String(date.getMonth() + 1).padStart(2, '0');
     const year = date.getFullYear();
     return `${day}-${month}-${year}`;
 }
-
 function volverAlPrincipal() {
     document.getElementById('registros').classList.remove('active');
     document.getElementById('listaRegistros').innerHTML = '';
-    document.getElementById('opciones').style.display = 'flex';
-    document.getElementById('tituloPrincipal').style.display = 'block';
+    document.getElementById('opciones').classList.remove('hidden');
+    document.getElementById('tituloPrincipal').classList.remove('hidden');
+    document.getElementById('resumenTitulo').classList.remove('hidden');  // Mostrar el título
 }
-
 function actualizarReloj() {
     const now = new Date();
     const horas = now.getHours() % 12 || 12;
@@ -262,7 +240,6 @@ function actualizarReloj() {
     const ampm = now.getHours() >= 12 ? 'PM' : 'AM';
     document.getElementById('reloj').innerText = `${horas}:${minutos}:${segundos} ${ampm}`;
 }
-
 function mostrarMensaje(opcion, mensaje) {
     document.getElementById(`mensaje${capitalize(opcion)}`).innerText = mensaje;
     setTimeout(() => {
@@ -270,7 +247,47 @@ function mostrarMensaje(opcion, mensaje) {
         cancelarRegistro();
     }, 3000);
 }
+function actualizarTablaRegistros() {
+    const entregaRegistros = JSON.parse(localStorage.getItem('entrega')) || [];
+    const finalizadoRegistros = JSON.parse(localStorage.getItem('finalizado')) || [];
 
+    const contarRegistrosPorUsuario = (registros) => {
+        return registros.reduce((contador, registro) => {
+            if (!contador[registro.nombre]) {
+                contador[registro.nombre] = { count: 0, totalTime: 0 };
+            }
+            contador[registro.nombre].count++;
+            contador[registro.nombre].totalTime += obtenerTiempoEnMinutos(registro.tiempoTotal);
+            return contador;
+        }, {});
+    };
+
+    const obtenerTiempoEnMinutos = (tiempoTotal) => {
+        const [horas, minutos] = tiempoTotal.split(' horas y ');
+        return parseInt(horas) * 60 + parseInt(minutos);
+    };
+
+    const calcularTiempoPromedio = (totalTime, count) => {
+        const promedio = totalTime / count;
+        const horas = Math.floor(promedio / 60);
+        const minutos = Math.floor(promedio % 60);
+        return `${horas} horas y ${minutos} minutos`;
+    };
+
+    const registrosSurtido = contarRegistrosPorUsuario(entregaRegistros);
+    const registrosEmpaque = contarRegistrosPorUsuario(finalizadoRegistros);
+
+    const crearFilasTabla = (registros) => {
+        return Object.entries(registros).map(([usuario, datos]) => {
+            const tiempoPromedio = calcularTiempoPromedio(datos.totalTime, datos.count);
+            return `<tr><td>${usuario}</td><td>${datos.count}</td><td>${tiempoPromedio}</td></tr>`;
+        }).join('');
+    };
+
+    document.querySelector('#tablaSurtido tbody').innerHTML = crearFilasTabla(registrosSurtido);
+    document.querySelector('#tablaEmpaque tbody').innerHTML = crearFilasTabla(registrosEmpaque);
+}
 window.onload = function() {
     setInterval(actualizarReloj, 1000);
+    actualizarTablaRegistros();  // Llamar a la función al cargar la página
 };
